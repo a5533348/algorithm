4fec5b858dfe830a78eeaf85391cc2f22bf9dafb
Monster <318531018@qq.com>
2020-01-01 22:14:27 +0800

    add 链表反转(递归)

--- vscode/.leetcode/206.反转链表(递归).java
+++ vscode/.leetcode/206.反转链表(递归).java
@@ -9,12 +9,21 @@
  * Definition for singly-linked list. public class ListNode { int val; ListNode
  * next; ListNode(int x) { val = x; } }
  * 
+ * 复杂度分析: 时间复杂度：O(n)，长度为n，需要递归n次； 空间复杂度：O(n)，由于使用递归，将会使用隐式栈空间。递归深度可能会达到n层。
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        
 
-        
+    // 首先根据递归终止条件，找到最后一个节点；
+    // 然后从尾部改变节点的指向
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return p;
     }
 }
 // @lc code=end
