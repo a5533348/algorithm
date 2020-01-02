d1a372c90c6a158b60460f11a9c4f0f6607a0519
Monster <318531018@qq.com>
2020-01-03 00:17:05 +0800

    add [283移动零]

---
+++ vscode/.leetcode/283.移动零.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        // 用j标记 遇到非0的时候要交换的位置
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+}
+// @lc code=end
